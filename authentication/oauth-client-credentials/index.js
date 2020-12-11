6b6cbed2f3d36cf1bdbef5047b037de0749436f7
Waldir Pimenta <waldir.pimenta@uphold.com>
2020-12-11 10:21:02 +0000

    Wrap the index files in try-catch blocks
    This allows for cleaner failure when one of the API calls fails

--- authentication/oauth-client-credentials/index.js
+++ authentication/oauth-client-credentials/index.js
@@ -5,10 +5,15 @@
 import { getAssets, getToken } from "./cc-flow.js";
 
 (async () => {
-  // Get `bearer` token from sandbox.
-  const token = await getToken();
+  try {
+    // Get `bearer` token from sandbox.
+    const token = await getToken();
 
-  // Test the new token by making a call to the API.
-  const assets = await getAssets(token);
-  console.log("Output from test API call:", assets[0]);
+    // Test the new token by making a call to the API.
+    const assets = await getAssets(token);
+    console.log("Output from test API call:", assets[0]);
+  } catch (error) {
+    // Unexpected error.
+    return;
+  }
 })();
--- authentication/personal-access-token/index.js
+++ authentication/personal-access-token/index.js
@@ -5,32 +5,37 @@
 import { createNewPAT, getAuthenticationMethods, getMyPATs } from "./cc-pat.js";
 
 (async () => {
-  // Get list of authentication methods.
-  const authMethods = await getAuthenticationMethods();
+  try {
+    // Get list of authentication methods.
+    const authMethods = await getAuthenticationMethods();
 
-  // In the Sandbox environment, the special OTP value `000000` can be passed for convenience.
-  const totp = {
-    OTPMethodId: "",
-    OTPToken: "000000",
-  };
+    // In the Sandbox environment, the special OTP value `000000` can be passed for convenience.
+    const totp = {
+      OTPMethodId: "",
+      OTPToken: "000000",
+    };
 
-  // Try to determine if the authenticated account has two-factor authentication (2FA) active,
-  // as that will require a one-time password (OTP) for the PAT creation request.
-  const totpCheck =
-    authMethods != null ? authMethods.find((x) => x.type === "totp") : null;
-  if (totpCheck) {
-    totp.OTPMethodId = totpCheck.id;
-  }
+    // Try to determine if the authenticated account has two-factor authentication (2FA) active,
+    // as that will require a one-time password (OTP) for the PAT creation request.
+    const totpCheck =
+      authMethods != null ? authMethods.find((x) => x.type === "totp") : null;
+    if (totpCheck) {
+      totp.OTPMethodId = totpCheck.id;
+    }
 
-  // Create a PAT.
-  const newPAT = await createNewPAT(totp);
+    // Create a PAT.
+    const newPAT = await createNewPAT(totp);
 
-  if (newPAT.accessToken) {
-    console.log("New Personal Access Token (PAT) created with success");
-    console.debug(newPAT.accessToken);
+    if (newPAT.accessToken) {
+      console.log("New Personal Access Token (PAT) created with success");
+      console.debug(newPAT.accessToken);
 
-    // Use the newly created PAT to list all PATs for this account.
-    console.log("List of available PATs");
-    console.log(await getMyPATs(newPAT.accessToken));
+      // Use the newly created PAT to list all PATs for this account.
+      console.log("List of available PATs");
+      console.log(await getMyPATs(newPAT.accessToken));
+    }
+  } catch (error) {
+    // Unexpected error.
+    return;
   }
 })();
